d888ac7b5839b099fe85e3c15a0e5ea97449ba33
sileiMiao <silei.Miao@dji.com>
2025-04-02 16:54:58 +0800

    m

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+import sphinx_rtd_theme
 project = 'test'
 copyright = '2025, test'
 author = 'test'
@@ -23,5 +23,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
